bd1d68cdd543debc017607ed8f88b04dbe14bb3f
volodie <119340399+volodie@users.noreply.github.com>
2023-06-13 12:13:48 +0300

    HomeWork5

---
+++ OtusHomeWork5/ClassRobot.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OtusHomeWork5
+{
+    interface IRobot
+    {
+        public void GetInfo()
+        {   
+            Console.WriteLine("I am a simple robot");
+        }
+        public void GetComponents()
+        {
+            Console.WriteLine("I am a simple robot");
+        }
+        public void GetRobotType()
+        {
+            Console.WriteLine("I am a simple robot");
+        }
+    }
+    interface IChargeable
+    {
+        public void Charge();
+        public string GetInfo();
+    }
+    interface IFlyingRobot : IRobot
+    {
+        public void GetRobotType()
+        {
+            Console.WriteLine("I am a flying robot");
+        }
+    }
+    class Quadcopter : IFlyingRobot, IChargeable
+    {
+        List<string> _components = new List<string> { "rotor1", "rotor2", "rotor3", "rotor4" };
+
+        void IRobot.GetComponents()
+        {
+            foreach (var item in _components)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        void IChargeable.Charge()
+        {
+            Console.WriteLine("Charging....");
+            Thread.Sleep(3000);
+            Console.WriteLine("Charged!");
+        }
+        string IChargeable.GetInfo()
+        {
+            string s = "Robot charging complete";
+            return s;
+        }
+    }
+    class Robot : IRobot
+    { }
+}
---
+++ OtusHomeWork5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Channels;
+using OtusHomeWork5;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        IRobot robot = new Robot();
+        robot.GetInfo();
+        robot.GetComponents();
+        robot.GetRobotType();
+
+        Quadcopter quadcopter = new Quadcopter();
+        ((IFlyingRobot)quadcopter).GetComponents();
+        ((IChargeable)quadcopter).Charge();
+        Console.WriteLine(((IChargeable)quadcopter).GetInfo()); 
+    }
+}
